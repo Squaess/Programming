e042023deb2f20af920903af925613fbe8786bc3
Squaess <babartosz.bns@gmail.com>
2017-04-27 22:11:18 +0200

    L4 AISD done

---
+++ AISD/L4/Binary_search_tree/src/main/BinaryTree.java
@@ -0,0 +1,166 @@
+package main;
+
+/**
+ * Created by Bartosz on 27.04.2017
+ */
+class BinaryTree<T extends Comparable<T>> {
+    long number_of_compare = 0;
+    Node<T> root;
+
+    /**
+     * konstruktor drzewa
+     */
+    BinaryTree()
+    {
+        root = null;
+    }
+
+    /**
+     * wyszykiwanie wartosci
+     * @param value wartosc
+     * @param up gdzie zaczac(root)
+     * @return wyszukany node, brak - null
+     */
+    Node<T> search(T value, Node<T> up) {
+        if(up!=null){
+            number_of_compare++;
+            if(up.getValue().compareTo(value)==0) {
+
+                return up;
+            } else {
+                number_of_compare++;
+                if (up.getValue().compareTo(value) >= 0) {
+
+                    return search(value, up.getLeft());
+                } else
+                    return search(value, up.getRight());
+            }
+        }
+        return null;
+    }
+    /**
+     * dodawanie wezla
+     * @param value wartosc
+     * @param up gdzie zaczassz przeszukiwanie(root)
+     */
+    void insert(T value, Node<T> up) {
+        if(root == null) root = new Node<>(value);
+        else{
+            if( up.getValue().compareTo(value) >= 0){
+                if(up.getLeft() != null)
+                    insert(value, up.getLeft());
+                else
+                    up.setLeft(new Node<>(value, up));
+            }
+            else if(up.getValue().compareTo(value) < 0){
+                if(up.getRight() != null)
+                    insert(value, up.getRight());
+                else
+                    up.setRight(new Node<>(value, up));
+            }
+        }
+    }
+    /**
+     * usowanie wezla
+     * @param up node ktory usunac
+     */
+    void delete(Node<T> up) {
+        if(up!=null){
+            if(up.getLeft()==null && up.getRight()==null){
+                if(up.getParent()==null) root=null;  //up is root
+                else if(up.getParent().getLeft()==up) up.getParent().setLeft(null);
+                else if(up.getParent().getRight()==up)up.getParent().setRight(null);
+            }
+            else if(up.getLeft()==null || up.getRight()==null){
+                if(up.getLeft()==null){
+                    if(up.getParent()==null){  //up is root
+                        root = up.getRight();
+                        root.setParent(null);
+                    }
+                    else if(up.getParent().getRight()==up){
+                        up.getParent().setRight(up.getRight());
+                        up.getRight().setParent(up.getParent());
+                    }
+                    else if(up.getParent().getLeft()==up)
+                    {
+                        up.getParent().setLeft(up.getRight());
+                        up.getRight().setParent(up.getParent());
+                    }
+                }
+                else if(up.getRight()==null){
+                    if(up.getParent()==null){//up is root
+                        root = up.getLeft();
+                        root.setParent(null);
+                    }
+                    else if(up.getParent().getRight()==up){
+                        up.getParent().setRight(up.getLeft());
+                        up.getLeft().setParent(up.getParent());
+                    }
+                    else if(up.getParent().getLeft()==up)
+                    {
+                        up.getParent().setLeft(up.getLeft());
+                        up.getLeft().setParent(up.getParent());
+                    }
+                }
+            }
+            else{
+                Node<T> help = minRight(up.getRight());
+                up.setValue(help.getValue());
+                delete(help);
+            }
+        }
+    }
+    /**
+     * wyszukiwanie najmniejszego nastepnika
+     * @param up gdzie zaczac(root)
+     * @return najwiekszey node z lewego drzewa up
+     */
+    private Node<T> minRight(Node<T> up){
+        if(up.getLeft()==null) return up;
+        else return minRight(up.getLeft());
+    }
+    /**
+     * rysowanie
+     * @param up gdzie zaczac(root)
+     *
+     */
+//    public void draw(Node<T> up, StringBuilder s) {
+//        if(up!=null){
+//            if(up.getLeft()!=null) draw(up.getLeft(),s);
+//            //System.out.print(up.getValue()+" ");
+//            s.append(" "+up.getValue());
+//            if(up.getRight()!=null) draw(up.getRight(),s);
+//        }
+//    }
+    void draw(Node<T> up) {
+        if(up!=null) {
+            if(up.getLeft()!=null) {
+                draw(up.getLeft());
+            }
+            System.out.print(up.getValue()+" ");
+            if(up.getRight() != null) draw(up.getRight());
+        }
+    }
+
+    void min() {
+        if(root == null) System.out.println("");
+        else {
+            Node n = root;
+            while(n.getLeft() != null) {
+                n = n.getLeft();
+            }
+            System.out.println(n.getValue());
+        }
+    }
+
+    void max(){
+        if(root == null) System.out.println("");
+        else {
+            Node n = root;
+            while(n.getRight() != null) {
+                n = n.getRight();
+            }
+            System.out.println(n.getValue());
+        }
+    }
+}
--- AISD/L4/Binary_search_tree/src/main/Main.java
+++ AISD/L4/Binary_search_tree/src/main/Main.java
@@ -1,10 +1,88 @@
 package main;
 
+import java.io.BufferedReader;
+import java.util.Random;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by aedd on 4/27/17
  */
 public class Main {
+    private static final String FILE_PATH = "./data.txt";
     public static void main(String[] args) {
-        System.out.println("Hello");
+//        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+//
+//        try {
+//            BufferedReader input = new BufferedReader(new FileReader(FILE_PATH));
+//            String line = input.readLine();
+//            int n = Integer.parseInt(line);
+//            for(int i = 0; i < n; i++) {
+//                line = input.readLine();
+//                String[] parts = line.split(" ");
+//                switch ( parts[0] ) {
+//                    case "insert"   :  // System.out.println("Insert "+ parts[1]);
+//                                        binaryTree.insert(Integer.parseInt(parts[1]),binaryTree.root);
+//                                        break;
+//                    case "delete"   :  // System.out.println("Delete " + parts[1]);
+//                                        Node<Integer> tmp = binaryTree.search(Integer.parseInt(parts[1]), binaryTree.root);
+//                                        binaryTree.delete(tmp);
+//                                        break;
+//                    case "min"      :  // System.out.println("min");
+//                                        binaryTree.min();
+//                                        break;
+//                    case "max"      :  // System.out.println("max");
+//                                        binaryTree.max();
+//                                        break;
+//                    case "inorder"  :  // System.out.println("in_order");
+//                                        binaryTree.draw(binaryTree.root);
+//                                        System.out.print("\n");
+//                                        break;
+//                    case "find"     :  // System.out.println("find " + parts[1]);
+//                                        if(binaryTree.search(Integer.parseInt(parts[1]), binaryTree.root) != null) System.out.println("1");
+//                                        else System.out.println("0");
+//                                        break;
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        experiment();
+    }
+
+    private static void experiment() {
+        int maximum_size_of_data = 100;
+        int number_of_test_for_search = 100000000;
+
+        for(int j = 0; j < maximum_size_of_data; j++) {
+
+            BinaryTree<Integer> bt = new BinaryTree<>();
+            long min = Integer.MAX_VALUE;
+            long max = 0;
+            long avg = 0;
+
+            /*
+              Ilosc wezłów dla naszego drzewa
+             */
+            Random r = new Random();
+
+            /*
+              Wypełnienie drzewa losowymi liczbami
+             */
+            for (int i = 0; i < j; i++) {
+                bt.insert(r.nextInt(), bt.root);
+            }
+
+            for(int k = 0; k < number_of_test_for_search; k++) {
+                bt.search(r.nextInt(), bt.root);
+                if(bt.number_of_compare > max) max = bt.number_of_compare;
+                if(bt.number_of_compare < min) min = bt.number_of_compare;
+                avg += bt.number_of_compare;
+                bt.number_of_compare = 0;
+            }
+            avg /= number_of_test_for_search;
+            System.out.println(j+ " " +min+ " " +avg+ " " +max);
+
+        }
     }
 }

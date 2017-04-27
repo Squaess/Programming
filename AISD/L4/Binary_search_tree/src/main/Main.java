33fe9d509934eb0fd8a9b77537bc1af2f7144bec
Squaess <babartosz.bns@gmail.com>
2017-04-27 19:27:57 +0000

    Starting Lab4 AISD project

---
+++ AISD/L4/Binary_search_tree/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by aedd on 4/27/17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ AISD/L4/Binary_search_tree/src/main/Node.java
@@ -0,0 +1,8 @@
+package main;
+
+/**
+ * Created by aedd on 4/27/17.
+ */
+public class Node {
+
+}
